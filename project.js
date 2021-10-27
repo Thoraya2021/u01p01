23ee49ab8a9713fb4723fa05093fb05d854b9169
Thoraya2021 <https://github.com/Thoraya2021/myFirstRepo>
2021-10-28 02:15:30 +0300

    pro01

--- project.js
+++ project.js
@@ -1,9 +1,16 @@
-let novels = [
+let novels = [{
+  name: "The Grapes of Wrath",
+  description:
+    "Author:John Steinbeck  ATOS reading level 4.90 First publishedApr 14, 1939 AdaptationsThe Grapes of Wrath (1940) · The Grapes of Wrath (1988)",
+  img: "https://th.bing.com/th/id/R.7ab14ea91f756f1a5ac276132c512a32?rik=wk1yPQx%2bMAUP9g&riu=http%3a%2f%2fww1.prweb.com%2fprfiles%2f2014%2f05%2f15%2f11858905%2fthe-grapes-of-wrath-original-dustjacket.jpg&ehk=34ja2%2fAVAMx4h7SNDm4Si1D9uzenVu0VarztVkPorw0%3d&risl=&pid=ImgRaw&r=0",
+  text: "Part one, Chapter one :TO THE RED COUNTRY and part of the gray country of Oklahoma, the last rains came gently, and they did not cut the scarred earth. The plows crossed and recrossed the rivulet marks. The last rains lifted the corn quickly and scattered weed colonies and grass along the sides of the roads so that the gray country and the dark red country began to disappear under a green cover. In the last part of May the sky grew pale and the clouds that had hung in high puffs for so long in the spring were dissipated. The sun flared down on the growing corn day after day until a line of brown spread along the edge of each green bayonet. The clouds appeared, and went away, and in a while they did not try any more. The weeds grew darker green to protect themselves, and they did not spread any more. The surface of the earth crusted, a thin hard crust, and as the sky became pale, so the earth became pale, pink in the red country and white in the gray country.In the water-cut gullies the earth dusted down in dry little streams. Gophers and ant lions started small avalanches. And as the sharp sun struck day after day, the leaves of the young corn became less stiff and erect; they bent in a curve at first, and then, as the central ribs of strength grew weak, each leaf tilted downward. Then it was June, and the sun shone more fiercely. The brown lines on the corn leaves widened and moved in on the central ribs. The weeds frayed and edged back toward their roots. The air was thin and the sky more pale; and every day the earth paled.In the roads where the teams moved, where the wheels milled the ground and the hooves of the horses beat the ground, the dirt crust broke and the dust formed. Every moving thing lifted the dust into the air: a walking man lifted a thin layer as high as his waist, and a wagon lifted the dust as high as the fence tops, and an automobile boiled a cloud behind it. The dust was long in settling back again. ",
+  favotite: false,
+},
   {
     name: "Nineteen Eighty-Four",
     description:
       "AuthorGeorge Orwell Number of pages328 First publishedJun 08, 1949 Preceded byAnimal Farm",
-    img: "https://th.bing.com/th/id/OIP.wdWqG4bLEBFIKAB3CsTPFgHaK5?pid=ImgDet&rs=1",
+    img: "https://www.themoviedb.org/t/p/original/brvLMSAJ3smt6Es6jZwZI07MY4X.jpg",
     text:"Part One It was a bright cold day in April, and the clocks were striking thirteen. Winston Smith, his chin nuzzled into his breast in an effort to escape the vile wind, slipped quickly through the glass doors of Victory Mansions, though not quickly enough to prevent a swirl of gritty dust from entering along with him.The hallway smelt of boiled cabbage and old rag mats. At one end of it a coloured poster, too large for indoor display, had been tacked to the wall. It depicted simply an enormous face, more than a metre wide: the face of a man of about forty-five, with a heavy black moustache and ruggedly handsome features. Winston made for the stairs. It was no use trying the lift. Even at the best of times it was seldom working, and at present the electric current was cut off during daylight hours.Inside the flat a fruity voice was reading out a list of figures which had something to do with the production of pig-iron. The voice came from an oblong metal plaque like a dulled mirror which formed part of the surface of the right-hand wall. Winston turned a switch and the voice sank somewhat, though the words were still distinguishable. The instrument (the telescreen, it was called) could be dimmed, but there was no way of shutting it off completely. He moved over to the window: a smallish, frail figure, the meagreness of his body merely emphasized by the blue overalls which were the uniform of the party. His hair was very fair, his face naturally sanguine, his skin roughened by coarse soap and blunt razor blades and the cold of the winter that had just ended."
 ,
     favotite: false,
@@ -22,24 +29,14 @@ let novels = [
     name: "To kill Mockingbird",
     description:
       "AuthorHarper Lee ATOS reading level5.60 First publishedJul 11, 1960 AdaptationsTo Kill a Mockingbird (1962)·",
-    img: "https://th.bing.com/th/id/OIP.FlCDOhM6yGlghOeaDZnn_gHaLj?pid=ImgDet&rs=1",
-    text: "",
+    img: "https://www.bibdsl.co.uk/imagegallery2/bds/201520/9781784870799.JPG",
+    text: " Part One, Chapter one:The story is narrated by a young girl named Jean Louise Finch, who is almost always called by her nickname, Scout. Scout starts to explain the circumstances that led to the broken arm that her older brother, Jem, sustained many years earlier; she begins by recounting her family history. The first of her ancestors to come to America was a fur-trader and apothecary named Simon Finch, who fled England to escape religious persecution and established a successful farm on the banks of the Alabama River. The farm, called Finch’s Landing, supported the family for many years. The first Finches to make a living away from the farm were Scout’s father, Atticus Finch, who became a lawyer in the nearby town of Maycomb, and his brother, Jack Finch, who went to medical school in Boston. Their sister, Alexandra Finch, stayed to run the Landing.",
     favotite: false,
-  },
+  }];
 
-  {
-    name: "The Grapes of Wrath",
-    description:
-      "Author:John Steinbeck  ATOS reading level 4.90 First publishedApr 14, 1939 AdaptationsThe Grapes of Wrath (1940) · The Grapes of Wrath (1988)",
-    img: "https://th.bing.com/th/id/R.7ab14ea91f756f1a5ac276132c512a32?rik=wk1yPQx%2bMAUP9g&riu=http%3a%2f%2fww1.prweb.com%2fprfiles%2f2014%2f05%2f15%2f11858905%2fthe-grapes-of-wrath-original-dustjacket.jpg&ehk=34ja2%2fAVAMx4h7SNDm4Si1D9uzenVu0VarztVkPorw0%3d&risl=&pid=ImgRaw&r=0",
-    text: "",
-    favotite: false,
-  },
-];
-localStorage.setItem("novels", JSON.stringify(novels));
-
-
-
+$(".add").click((i)=>{
+localStorage.setItem("novels", JSON.stringify(novels[i]))
+});
 
 const render = () => {
   $(".cards").html("");
